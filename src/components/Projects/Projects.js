205d79e612604889c91fd4233f97efbd58b2dbaf
harshalkale0911 <hkale6888@gmail.com>
2024-07-26 15:15:39 +0530

    update portfolio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -99,9 +99,12 @@ import Particle from "../Particle";
 import RelianceWorld from "../../Assets/Projects/RelianceWorld.png";
 import editor from "../../Assets/Projects/editor.png";
 import petpal from "../../Assets/Projects/petpal.png";
-import jsLogo from "../../Assets/Logos/js-logo.png"; // Add the logo paths
-import reactLogo from "../../Assets/Logos/react-logo.png";
-import nodeLogo from "../../Assets/Logos/node-logo.png";
+import jsLogo from "../../Assets/jsLogo.jpeg"; // Add the logo paths
+import reactLogo from "../../Assets/reactLogo.jpeg";
+import nodeLogo from "../../Assets/nodeLogo.jpeg";
+import cssLogo from "../../Assets/cssLogo.jpeg";
+import HtmlLogo from "../../Assets/HtmlLogo.jpeg";
+//import bootstrp from "../../Assets/bootstrp.jpeg"
 
 function Projects() {
   return (
@@ -123,7 +126,7 @@ function Projects() {
               description="The Reliance World website serves as an online platform where customers can browse through their product catalog, check specifications, compare prices, and make online purchases. It provides a convenient way for customers to explore and buy electronic goods from the comfort of their homes."
               ghLink="https://github.com/itiaditi/javascript-jadoo-8901"
               demoLink="https://65ade3de9fb39fdcb10c705e--stirring-starlight-16f55e.netlify.app/"
-              techLogos={[jsLogo, reactLogo, nodeLogo]}
+              techLogos={[ cssLogo ,  HtmlLogo ]}
             />
           </Col>
 
@@ -135,7 +138,7 @@ function Projects() {
               description="It was a collaborative effort taken by 5 team members and it a clone of Frank And Oak famous shopping website. Card creation from the backend data, sorting functions on price ASC and DSC, responsiveness, Admin Panel, Authentication."
               ghLink="https://github.com/AdityaSingh-byte/Frank-and-Oak"
               demoLink="https://frankandoak.netlify.app/"
-              techLogos={[jsLogo, reactLogo, nodeLogo]}
+              techLogos={[ jsLogo , HtmlLogo , cssLogo ]}
             />
           </Col>
 
@@ -147,7 +150,7 @@ function Projects() {
               description="This website is designed to facilitate the adoption of pets, such as dogs and cats, from shelters or rescue organizations. The platform serves as a digital marketplace where potential adopters can browse and select pets, shelters can list available pets, and donors can contribute to support animal welfare."
               ghLink="https://github.com/Sartik0709/Panasonic-Pioneers_062"
               demoLink="https://pet-petpals.netlify.app/"
-              techLogos={[jsLogo, reactLogo, nodeLogo]}
+              techLogos={[jsLogo ,  nodeLogo , HtmlLogo , cssLogo  , reactLogo]}
             />
           </Col>
         </Row>
